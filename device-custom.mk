3d0d7f8d78761e2df976703c00fb23cd8048f75d
jhenrique09 <jhsv09@gmail.com>
2022-02-09 12:35:22 -0300

    redfin: Move some stuff to common tree

--- device-custom.mk
+++ device-custom.mk
@@ -9,11 +9,6 @@ $(call inherit-product, device/google/redbull/device-custom.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-custom
 
-# Quick tap gesture
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.columbus.use_ap_sensor=false \
-    persist.columbus.model=tap7cls_redfin.tflite
-
 # HBM
 PRODUCT_PACKAGES += \
     HbmSVManagerOverlay
