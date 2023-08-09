b534a79ec6c41ab37849a09c117116785abc001c
jhenrique09 <jhsv09@gmail.com>
2023-08-09 22:18:43 +0000

    redfin: convert to RRO
    Change-Id: Ic5cc6abe793b2863eb7afea4aa4f055a3a0bef24

--- device-custom.mk
+++ device-custom.mk
@@ -6,12 +6,14 @@
 
 $(call inherit-product, device/google/redbull/device-custom.mk)
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-custom
-
 # HBM
 PRODUCT_PACKAGES += \
     HbmSVManagerOverlay
 
 # wireless_charger HAL service
 include device/google/redfin/wireless_charger/wireless_charger.mk
+
+# Overlays
+PRODUCT_PACKAGES += \
+    RedfinSystemUIOverlay \
+    RedfinSettingsOverlay
